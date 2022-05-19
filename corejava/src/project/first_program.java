13cbf7cb572dd17a7ccf411429922ee36d45f04e
RAJKUMAR RK <RAJKUMAR RK@DESKTOP-4KP7DEL>
2022-05-19 15:12:50 +0530

    first_program

---
+++ corejava/src/project/first_program.java
@@ -0,0 +1,15 @@
+package project;
+
+public class first_program {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=10;
+		int b=20;
+		short c=350;
+   System.out.println("sum of A+b="+(a+b));
+   System.out.println("c="+c);
+   
+	}
+
+}
