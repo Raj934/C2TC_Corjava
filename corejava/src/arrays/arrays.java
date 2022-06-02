bbbf8d58110049b97a30a13d4a55f915d00589fe
RAJKUMAR RK <RAJKUMAR RK@DESKTOP-4KP7DEL.alphionsee.in>
2022-06-02 15:53:55 +0530

    2/06/2022

---
+++ corejava/src/arrays/arrays.java
@@ -0,0 +1,19 @@
+package arrays;
+
+public class arrays {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int a[]= {10,20,30,40,50};
+		int sum=0;
+		
+		for(int i=0;i<a.length;i++)
+		{
+			sum+=a[i];
+			
+		}
+		System.out.println("sum of the array is ="+sum);
+	}
+
+}
